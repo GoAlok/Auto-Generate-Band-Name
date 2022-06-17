f63256d1e4b57a7d06908dd85dd6b1faf421e25b
GoAlok <lovingalokpd@gmail.com>
2022-06-17 10:14:48 +0530

    main

---
+++ autoGenereateBandName.py
@@ -0,0 +1,11 @@
+# 1. Create a greeting for your program.
+print("--------*** Welcome To The Auto Brand Name Generator ***--------")
+# 2. Ask user for the city they grew up.
+user_city = (input("In which city tou grow up?\n")).capitalize()
+# 3. Ask user for the name of a pet.
+pet_name = (input("What's your pet name?\n")).capitalize()
+# 4. Combine the name of their city and pet to show them their band name.
+print("your Band Name could be : "+ user_city+" "+ pet_name)
+# 5. NOTE: Make sure the input cursor shows on the new line.
+
+# RESULT IS @ https://github.com/GoAlok/AutoGenerateBandName/blob/main/autoGenereateBandName.py
